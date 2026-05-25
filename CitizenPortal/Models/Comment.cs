d6cf3eea6bfb6d435497b0083531bef4b0a7a618
Boran Barutçu <borannbarutcu@gmail.com>
2026-05-25 20:06:40 +0200

    Fixed syntax error

--- CitizenPortal/Models/Comment.cs
+++ CitizenPortal/Models/Comment.cs
@@ -2,7 +2,7 @@
 {
     public class Comment
     {
-        public int CommentId { get; set; }
+        public int Id { get; set; }
         public int UserId { get; set; }
         public int ApplicationId { get; set; }
         public string Text { get; set; } =string.Empty;
